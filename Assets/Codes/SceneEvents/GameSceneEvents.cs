bf26ae1b0b628a2f58a20021aedf05ac31b4bcdc
OrangeKnife <evisd21@gmail.com>
2015-05-11 00:54:52 -0400

    remove useless folder and fix a minor issue about debug log

--- Assets/Codes/SceneEvents/GameSceneEvents.cs
+++ Assets/Codes/SceneEvents/GameSceneEvents.cs
@@ -677,7 +677,6 @@ public class GameSceneEvents : MonoBehaviour {
 	public void ToggleDebug()
 	{
 		Utils.bDebug = !Utils.bDebug;
-		gameMgr.SendRepeatingNotification ();
 	}
 
 	public void onAutoMessageOKButtonClicked()
